beb056117e94cbbe0f08c06cd8a893bb212e77b3
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2019-04-30 12:09:30 -0700

    Yeet part 3

---
+++ src/footer.php
@@ -0,0 +1,23 @@
+<style type="text/css">
+	#yeet {
+		max-height: 50px;
+		background-color: #2060C7;
+	}
+	.nav-link {
+		color: !important rgba(255,255,255,.5);
+	}
+</style>
+
+<nav class='navbar fixed-bottom' id='yeet'>
+	<div class="collapse navbar-collapse" id="nav-content">   
+		<ul class="navbar-nav">
+			<li class="nav-item ">
+				<a class=" fixed-bottom nav-link" style="color: white;" href="/">Legal</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/about">Credit</a>
+			</li>
+		</ul>
+
+	</div>
+</nav>
